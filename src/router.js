be5cc2e37fa3f40277bc424cd4f333ba1a8f9f89
David <easyvalley@163.com>
2018-03-14 12:57:31 +0800

    update

--- src/router.js
+++ src/router.js
@@ -2,14 +2,22 @@ const Router = require('koa-router');
 const path = require('path');
 const walk = require('./readfile');
 const sizeOf = require('image-size');
+const fs = require('fs');
 
 const router = new Router();
 
+router.get('/', async (ctx, next) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream(path.join(__dirname, './index.html'));
+});
 router.get('/images', async (ctx, next) => {
 
     var result = walk(path.join(__dirname, '../courseimg'));
+    
+    result = result.filter((item) => {
+        return (item || '').indexOf('.DS_Store') < 0;
 
-    result = result.slice(1);
+    });
 
     result = result.map((item) => {
         var dimensions = sizeOf(item);
@@ -19,7 +27,8 @@ router.get('/images', async (ctx, next) => {
         return {
             name: str,
             w: dimensions.width,
-            h: dimensions.height
+            h: dimensions.height,
+            url:temArr[1]
         };
     });
     ctx.type = 'json';
--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 const Koa = require('koa');
 const fs = require('fs');
 const path = require('path');
+const staticServer = require('koa-static');
 const app = new Koa();
+
 
 const router = require('./router');
 
 function start() {
     app.use(router.routes());
     app.use(router.allowedMethods());
-    // app.use(async ctx => {
-    //     var r = walk(path.join(__dirname, '../courseimg'));
-    //     console.log(r);
-    //     ctx.body = 'Hello World koa';
-    // });
+    app.use(staticServer(path.join(__dirname, '../courseimg')));
 
     app.listen(8001, () => {
         console.log(`server is running at port 8001`);
