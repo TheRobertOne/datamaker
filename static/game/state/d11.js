295aded06d50776f6caaf9fff7d2a86d414e952e
EasyValley <pingyiluo@gmail.com>
2018-04-12 11:02:42 +0800

    upate

--- static/game/state/d11.js
+++ static/game/state/d11.js
@@ -5,17 +5,21 @@ class D11 {
 
     }
     preload(game) {
+        game.scale.pageAlignHorizontally = true; //水平居中
+        game.scale.pageAlignVertically = true; //垂直居中
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
         game.load.image('carrot', require('../assets/sprites/carrot.png'));
         game.load.image('star', require('../assets/sprites/star_particle.png'));
         game.load.image('diamon', require('../assets/sprites/diamond.png'));
     }
     create(game) {
         game.stage.disableVisibilityChange = true;
-        this.emitter = game.add.emitter(game.world.centerX, game.world.centerY, 200);
+        this.emitter = game.add.emitter(game.world.centerX, game.world.centerY, 1000);
        
         window.ee = game.add.emitter;
         this.emitter.makeParticles(['carrot', 'star', 'diamon']);
-        this.emitter.start(false, 20000, 20);
+        this.emitter.start(false);
    
 
     }
