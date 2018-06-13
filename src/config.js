40935286f30a0636ff3af42b41dfa6b60c899c91
EasyValley <pingyiluo@gmail.com>
2018-06-13 13:09:43 +0800

    upate

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     isDevLesson: true,//开发课件？
     isOldReact: false,
     isFinal: true,
-    kejianPath: `/Users/pingyiluo/Desktop/Courseware/appdata/21days/day14`,
+    kejianPath: `/Users/pingyiluo/Desktop/Courseware/appdata/21days/day2`,
     is169: true
 };
